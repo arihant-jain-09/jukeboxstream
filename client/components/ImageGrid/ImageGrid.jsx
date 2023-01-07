334064c56d8d863de130af53ffe6e829deb40518
Arihant Jain <jain.ari2000@gmail.com>
2023-01-07 18:35:26 +0530

    signed .m3u8 segments and send a body response

--- client/components/ImageGrid/ImageGrid.jsx
+++ client/components/ImageGrid/ImageGrid.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ImageGrid.module.scss";
 const ImageGrid = ({ items }) => {
+  const [currentSong, setCurrentSong] = useState(null);
+
+  const handleClick = (item) => {
+    const { s3Name } = item;
+    const folderName = s3Name.substr(0, s3Name.lastIndexOf("."));
+    const m3u8FilePath = `${folderName}/${folderName}.m3u8`;
+  };
+
   return (
     <div className={styles["imageGrid"]}>
       {items &&
-        items.map((item) => (
-          <img
-            className={styles["imageGrid-item"]}
-            src={item.cover}
-            alt="not found"
-          />
-        ))}
+        items.map((item) => {
+          const {
+            title: { S: titleName },
+            artist: { S: artistName },
+          } = item;
+          return (
+            <div
+              className={styles["imageGrid-item"]}
+              onClick={() => handleClick(item)}
+            >
+              <img
+                className={styles["imageGrid-item--img"]}
+                src={item.cover}
+                alt="not found"
+              />
+              <div className={styles["imageGrid-item--title"]}>{titleName}</div>
+              <div className={styles["imageGrid-item--artist"]}>
+                {artistName}
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
--- routes/streamRoutes.js
+++ routes/streamRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { db } from "../services/dynamoDB.js";
-import { ScanCommand } from "@aws-sdk/client-dynamodb";
+import { GetItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { getSignedUrl as cloudfrontSignedUrl } from "@aws-sdk/cloudfront-signer";
 import * as fs from "fs";
+import https from "https";
+import path from "path";
 
 const router = express.Router();
 
@@ -35,6 +37,119 @@ router.get("/streams/all", async (req, res) => {
     console.log(`${folderName}/${image.s3Name}`);
   }
   res.send(images);
+});
+
+router.get("/streams/:id", async (req, res) => {
+  const musicId = req.params.id;
+  console.log(musicId);
+  const command = new GetItemCommand({
+    AttributesToGet: ["s3Name"],
+    TableName: "streams",
+    Key: {
+      id: { S: musicId },
+    },
+  });
+
+  const { Item } = await db.send(command);
+  if (Item) {
+    const {
+      s3Name: { S: s3Name },
+    } = Item;
+    const folderName = s3Name.substr(0, s3Name.lastIndexOf("."));
+    const s3ObjectKey = `${folderName}/${folderName}.m3u8`;
+    const url = `${process.env.CLOUDFRONT_DOMAIN}/${s3ObjectKey}`;
+    const privateKey = fs.readFileSync(
+      new URL("../private_key.pem", import.meta.url),
+      {
+        encoding: "utf8",
+      }
+    );
+    const keyPairId = process.env.CLOUDFRONT_KEY_PAIR_ID;
+    const dateLessThan = new Date(new Date().getTime() + 60 * 60000);
+    const m3u8Url = cloudfrontSignedUrl({
+      url,
+      keyPairId,
+      dateLessThan,
+      privateKey,
+    });
+    let response = {};
+
+    function signed(url) {
+      const pathInS3 = `${folderName}/${url}`;
+      const tsSigned = `${process.env.CLOUDFRONT_DOMAIN}/${pathInS3}`;
+      let signedUrl = cloudfrontSignedUrl({
+        url: tsSigned,
+        keyPairId,
+        dateLessThan,
+        privateKey,
+      });
+      let qry = signedUrl.split("?").pop();
+      return url + "?" + qry;
+    }
+
+    https
+      .get(m3u8Url, (resp) => {
+        response.status = resp.statusCode;
+        let headers = {
+          "content-type": [
+            {
+              key: "Content-Type",
+              value: resp.headers["content-type"] || "text/html",
+            },
+          ],
+          server: [
+            { key: "Server", value: resp.headers["server"] || "Server" },
+          ],
+        };
+        response.headers = headers;
+        let data = "";
+
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        let body = [];
+
+        resp.on("end", () => {
+          data.split("\n").forEach((elem) => {
+            if (elem.startsWith("#")) {
+              if (elem.indexOf("URI") != -1) {
+                //URI component inline
+                var uriComponents = elem
+                  .substring(elem.indexOf("URI"))
+                  .split('"');
+
+                uriComponents[1] = signed(uriComponents[1]);
+                body.push(
+                  elem.substring(0, elem.indexOf("URI")) +
+                    uriComponents.join('"')
+                );
+              } else {
+                body.push(elem);
+              }
+            } else if (elem == "") {
+              body.push(elem);
+            } else {
+              let modifiedUrl = signed(elem);
+              body.push(modifiedUrl);
+            }
+          });
+          response.body = body.join("\n");
+          res.send(response);
+        });
+      })
+      .on("error", (err) => {
+        res.send({
+          status: "500",
+          statusDescrition: "Server Error",
+          headers: {
+            "content-type": [{ key: "Content-Type", value: "text/plain" }],
+          },
+          body: "Error reading content \n\n" + err,
+        });
+      });
+    // res.send(response);
+  } else res.send({ message: "resource not found" });
 });
 
 export default router;
