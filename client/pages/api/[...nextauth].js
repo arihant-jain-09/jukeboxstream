304ec2770083ef5f8fe5c066753da8db4a156dbd
Arihant Jain <jain.ari2000@gmail.com>
2023-01-26 11:45:03 +0530

    changed env

--- client/pages/api/[...nextauth].js
+++ client/pages/api/[...nextauth].js
@@ -24,7 +24,7 @@ export default NextAuth({
     CognitoProvider({
       clientId: process.env.COGNITO_CLIENT_ID,
       clientSecret: process.env.COGNITO_CLIENT_SECRET,
-      issuer: process.env.COGNITO_DOAMIN,
+      issuer: process.env.COGNITO_DOMAIN,
     }),
   ],
   secret: process.env.JWT_SECRET,
