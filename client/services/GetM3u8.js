76c263985bff77c484b123f0e0fb5edb45635451
Arihant Jain <jain.ari2000@gmail.com>
2023-07-26 23:53:51 +0530

    changed m3u8 get for prod too

--- client/services/GetM3u8.js
+++ client/services/GetM3u8.js
@@ -1,5 +1,14 @@
-import { BASE_GET_SONG } from "../utils/api-end-points";
+import { BASEAPI, BASE_GET_SONG } from "../utils/api-end-points";
+import awsconfig from "../aws-exports"
 import http from "./HttpService";
 export function getM3u8(itemId) {
-  return http.get(`${BASE_GET_SONG}/${itemId}`);
+  if(process.env.NEXT_PUBLIC_ENV == "dev"){
+  //   return http.get(`${BASEAPI}/${BASE_GET_SONG}/${itemId}`);
+  }
+  else{
+    const arr_of_routes = awsconfig.aws_cloud_logic_custom
+    const endpoint = arr_of_routes.filter((route)=>route.name=="streams")[0].endpoint
+    return http.get(`${endpoint}/${BASE_GET_SONG}/${itemId}`);
+  }
+  
 }
